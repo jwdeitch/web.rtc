ecfea4f37d42237eaccf486f614399e52e8cbd89
jwdeitch <jwdeitch@gmail.com>
2016-05-14 19:20:35 -0400

    logging

--- server.js
+++ server.js
@@ -21,21 +21,32 @@ httpServer.listen(port, () => {
     console.log(`Boot on http://localhost:${port}`);
 });
 
+io.sockets.on('connection', function (socket) {
+    var address = socket.handshake.address;
+    console.log('New connection from ' + address.address + ':' + address.port);
+});
+
+io.sockets.on('disconnect', function (socket) {
+    var address = socket.handshake.address;
+    console.log('Lost connection from ' + address.address + ':' + address.port);
+});
+
 // Manage p2p keys
 const keys = [];
 peerServer.on('connection', (key) => {
     keys.push(key);
 
-    console.log('connected', keys);
+    console.log('connected', key);
 
     io.emit('keys', keys);
 });
+
 peerServer.on('disconnect', (key) => {
     const index = keys.indexOf(key);
     if (index > -1) {
         keys.splice(index, 1);
     }
-    console.log('disconnect', keys);
+    console.log('disconnect', key);
 
     io.emit('keys', keys);
 });
