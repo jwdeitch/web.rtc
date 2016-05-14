f6ea29b1ecc6cd48ae2dd1234458192bcb8d63e8
jwdeitch <jwdeitch@gmail.com>
2016-05-14 19:16:15 -0400

    cleanup

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const createIo = require('socket.io');
 const createPeerServer = require('peer').ExpressPeerServer;
 
 // Environment
-const port = process.env.PORT || 59798;
+const port = process.env.PORT || 8080;
 
 // Routes
 const app = express();
@@ -18,24 +18,24 @@ app.use(express.static(__dirname));
 
 // Boot
 httpServer.listen(port, () => {
-  console.log(`Boot on http://localhost:${port}`);
+    console.log(`Boot on http://localhost:${port}`);
 });
 
 // Manage p2p keys
 const keys = [];
 peerServer.on('connection', (key) => {
-  keys.push(key);
+    keys.push(key);
 
-  console.log('connected', keys);
+    console.log('connected', keys);
 
-  io.emit('keys', keys);
+    io.emit('keys', keys);
 });
 peerServer.on('disconnect', (key) => {
-  const index = keys.indexOf(key);
-  if (index > -1) {
-    keys.splice(index, 1);
-  }
-  console.log('disconnect', keys);
+    const index = keys.indexOf(key);
+    if (index > -1) {
+        keys.splice(index, 1);
+    }
+    console.log('disconnect', keys);
 
-  io.emit('keys', keys);
+    io.emit('keys', keys);
 });
